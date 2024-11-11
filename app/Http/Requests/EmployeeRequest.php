6aa66242834b87905b9964723279b52a4a2848db
AttilaBS <attila.barcellos.sipos@gmail.com>
2024-11-11 17:19:42 -0300

    [Update] Added employee form requests

---
+++ app/Http/Requests/EmployeeRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\EmailPattern;
+use App\Rules\NamePattern;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+final class EmployeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string,array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'between:3,120', new NamePattern],
+            'cpf' => 'required|string|size:11',
+            'city' => 'required|string|between:3,120',
+            'state' => 'required|string|between:3,120',
+            'email' => [
+                'required',
+                'string',
+                'email',
+                new EmailPattern,
+                Rule::unique('employees', 'email')
+                    ->ignore($this->uuid, 'id'),
+            ],
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge(
+            [
+                'cpf' => preg_replace(
+                    '/[^0-9]/is',
+                    '',
+                    $this->input('cpf')
+                ),
+            ]
+        );
+    }
+}
---
+++ app/Http/Requests/UploadEmployeesListRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class UploadEmployeesListRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'list' => 'required|mimes:csv',
+        ];
+    }
+}
