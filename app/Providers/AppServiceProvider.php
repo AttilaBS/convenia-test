1859bdaac2d584681f8704e587224e64492a9da9
AttilaBS <attila.barcellos.sipos@gmail.com>
2024-11-13 13:15:06 -0300

    [Improvement] Added rate limit at user and api requests, updated php to 8.3 and added the limiter to api routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -26,5 +29,31 @@ class AppServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(now()->addDays(20));
         Passport::refreshTokensExpireIn(now()->addDays(40));
         Passport::personalAccessTokensExpireIn(now()->addMonths(3));
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(
+                $request->user()?->id
+                    ? $request->user()->id
+                    : $request->ip()
+            );
+        });
+
+        RateLimiter::for('user', function (Request $request) {
+            return Limit::perMinutes(2, 3)->by($request->ip())->response(
+                function (Request $request) {
+                    logger()->info('Foi excedido o número máximo de tentativas pelo usuário:', [
+                        'email: ' => $request->input('email'),
+                        'ip: ' => $request->ip(),
+                    ]);
+
+                    return response()->json(
+                        [
+                            'message' => 'O número máximo de tentativas foi excedido. Favor tentar novamente em alguns minutos',
+                            'status' => 429,
+                        ],
+                    );
+                }
+            );
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,22 @@ use App\Http\Controllers\User\LoginUserController;
 use App\Http\Controllers\User\LogoutUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', CreateUserController::class);
-Route::post('login', LoginUserController::class);
+Route::get('/status', function (): string {
+        return 'ok';
+    }
+    );
+
+Route::middleware('throttle:user')->name('user.')->prefix('user')
+    ->group(
+        function (): void {
+            Route::post('register', CreateUserController::class);
+            Route::post('login', LoginUserController::class);
+            Route::middleware('auth:api')->group(function(): void {
+                Route::get('logout', LogoutUserController::class);
+            });
+        });
 
 Route::middleware('auth:api')->group(function (): void {
-    Route::get('logout', LogoutUserController::class);
-
     Route::name('employee.')->prefix('employee')
         ->group(
             function (): void {
