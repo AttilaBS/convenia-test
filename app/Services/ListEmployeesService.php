3ead4d8e3f8338fa5250ce5e1e058c491a0e8e2c
AttilaBS <attila.barcellos.sipos@gmail.com>
2024-11-13 16:07:07 -0300

    [Improvement] Added dinamic caching at employees query listing with 60 seconds life span

--- app/Services/ListEmployeesService.php
+++ app/Services/ListEmployeesService.php
@@ -4,13 +4,16 @@ namespace App\Services;
 
 use App\Models\Employee;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class ListEmployeesService
 {
     public function __invoke(): Collection
     {
-        return app(Employee::class)
-            ->where('manager_id', auth()->user()->id)
-            ->get();
+        return Cache::remember('employees-listing', 60, function () {
+            return app(Employee::class)
+                ->where('manager_id', auth()->user()->id)
+                ->get();
+        });
     }
 }
